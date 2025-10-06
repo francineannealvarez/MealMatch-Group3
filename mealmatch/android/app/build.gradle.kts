996acdd7dc24f32c37ad9e265a4ff52cf3e44629
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2025-10-06 19:09:15 +0800

    screens and firebase connection

--- mealmatch/android/app/build.gradle.kts
+++ mealmatch/android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +42,13 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // ✅ Import the Firebase Bill of Materials (BoM)
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+    // ✅ Add the Firebase SDKs you’ll use
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth") // Optional, for login later
 }
